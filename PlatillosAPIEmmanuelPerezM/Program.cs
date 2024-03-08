ecd703790c8ee080e7cf24fbb605dfc9103866cf
MREmmanuel09 <128647995+MREmmanuel09@users.noreply.github.com>
2024-03-08 10:09:06 -0700

    Add project files.

---
+++ PlatillosAPIEmmanuelPerezM/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using PlatillosAPIEmmanuelPerezM.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        var CnnStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("CNNSTR"));
+
+        CnnStrBuilder.Password = "progra6";
+
+        string CnnStr = CnnStrBuilder.ConnectionString;
+
+
+       builder.Services.AddDbContext<PlatillosDbContext>(options => options.UseSqlServer(CnnStr));
+       //builder.Services.AddDbContext<PlatillosAPIEmmanuelPerezM.PlatillosAPIEmmanuelPerezM>(options => options.UseSqlServer(CnnStr));
+
+
+
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseRouting(); 
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
